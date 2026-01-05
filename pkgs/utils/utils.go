fc0592fe50856d1a1827d079bc8b544dd0a2972a
Muhammadbobur <bobur0218programmer@gmail.com>
2026-01-05 20:04:09 +0500

    fix: remove AllowCredentials: true from the cors config

--- pkgs/utils/utils.go
+++ pkgs/utils/utils.go
@@ -33,12 +33,10 @@ func InitRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+		MaxAge:          12 * time.Hour,
 	}))
 
 	return router
